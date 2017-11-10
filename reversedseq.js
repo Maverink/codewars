136b3cbc34dc809ca8305a94dc6814ff2d00e207
Maverink <juliomaurocarvalho@hotmail.com>
2017-11-10 23:55:36 +0000

    solving reversedseq.js

---
+++ reversedseq.js
@@ -0,0 +1,17 @@
+/*Get the number n to return the reversed sequence from n to 1.
+
+Example : n=5 >> [5,4,3,2,1]
+
+*/
+
+const reverseSeq = n => {
+outputArr = [];
+
+for(i=n;i>0;i--){
+
+outputArr.push(i);
+
+}
+
+  return outputArr;
+};

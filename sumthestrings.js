2a8c417909bf8936203c0811a8f297d7cac0b598
Maverink <juliomaurocarvalho@hotmail.com>
2017-11-16 02:06:11 +0000

    solving sumthestrings.js

---
+++ sumthestrings.js
@@ -0,0 +1,11 @@
+function sumStr(a,b) {
+
+  if(a==="" &&  b===""){
+     return "0";}
+     else if(a==="")
+     { a=0;} 
+     else if( b===""){
+      b=0};
+return(parseInt(a) + parseInt(b)).toString();
+}
+  

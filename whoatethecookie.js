930ebf414b9037aa0063f590f5ac671f8d150d1d
Maverink <juliomaurocarvalho@hotmail.com>
2017-12-21 23:50:43 +0000

    solving whoatethecookie.js

---
+++ whoatethecookie.js
@@ -0,0 +1,9 @@
+function cookie(x){
+if(typeof x == 'string'){
+  return 'Who ate the last cookie? It was Zach!';
+}else if (typeof x == 'number'){
+  return "Who ate the last cookie? It was Monica!";
+} else{
+  return "Who ate the last cookie? It was the dog!";
+}
+}

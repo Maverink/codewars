63ad5f6be48fef8c80e40ad3f31f83fe6752defb
Maverink <juliomaurocarvalho@hotmail.com>
2018-01-09 11:30:53 +0000

    solving intdepth.js

---
+++ intdepth.js
@@ -0,0 +1,50 @@
+// The depth of an integer n is defined to be how many multiples of
+//  n it is necessary to compute before all 10 digits have appeared at
+//   least once in some multiple.
+
+// example:
+
+// let see n=42
+
+// Multiple         value         digits     comment
+// 42*1              42            2,4
+// 42*2              84             8         4 existed
+// 42*3              126           1,6        2 existed
+// 42*4              168            -         all existed
+// 42*5              210            0         2,1 existed
+// 42*6              252            5         2 existed
+// 42*7              294            9         2,4 existed
+// 42*8              336            3         6 existed
+// 42*9              378            7         3,8 existed
+// Looking at the above table under digits column you can find all the digits
+// from 0 to 9, Hence it required 9 multiples of 42 to get all the digits.
+//  So the depth of 42 is 9. Write a function named computeDepth
+//  which computes the depth of its integer argument.Only positive numbers
+//  greater than zero will be passed as an input.
+
+function computeDepth(x) {
+	var range1 = "0123456789";
+	var range2 = "";
+	var depth = 0;
+	var currentX;
+
+	while (range2.length < range1.length) {
+		depth++;
+		currentX = (x * depth).toString().split("");
+
+		for (i = 0; i < currentX.length; i++) {
+			if (range2.indexOf(currentX[i]) == -1) {
+				range2 += currentX[i];
+			}
+		}
+	}
+	return depth;
+}
+
+
+// describe("Integer depth", _=>{
+//   it("sample tests", _=>{
+//   Test.assertDeepEquals(computeDepth(1),10);
+//   Test.assertDeepEquals(computeDepth(42),9);
+//   });
+// });

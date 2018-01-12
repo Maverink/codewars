cb41aecc497ce5292f528afe1bfe1cf75c95ec67
Maverink <juliomaurocarvalho@hotmail.com>
2018-01-12 08:48:26 +0000

    solving buildtower.js

---
+++ buildtower.js
@@ -0,0 +1,45 @@
+// Build Tower
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+
+// Tower block is represented as *
+
+// Python: return a list;
+// JavaScript: returns an Array;
+// C#: returns a string[];
+// PHP: returns an array;
+// C++: returns a vector<string>;
+// Haskell: returns a [String];
+// Ruby: returns an Array;
+// Have fun!
+
+// for example, a tower of 3 floors looks like below
+
+// [
+//   '  *  ', 
+//   ' *** ', 
+//   '*****'
+// ]
+// and a tower of 6 floors looks like below
+
+// [
+//   '     *     ', 
+//   '    ***    ', 
+//   '   *****   ', 
+//   '  *******  ', 
+//   ' ********* ', 
+//   '***********'
+// ]
+
+function towerBuilder(nFloors) {
+	var output = [];
+
+	for (i = 0, j = nFloors; i < nFloors; i++, j--) {
+		output.unshift(" ".repeat(i) + "*".repeat(j * 2 - 1) + " ".repeat(i));
+	}
+	return output;
+}
+
+// Test.assertEquals(JSON.stringify(towerBuilder(1)), JSON.stringify(["*"]));
+// Test.assertEquals(JSON.stringify(towerBuilder(2)), JSON.stringify([" * ","***"]));
+// Test.assertEquals(JSON.stringify(towerBuilder(3)), JSON.stringify(["  *  "," *** ","*****"]));

43d6306f9098972d5c2191c54a95101c25177463
Maverink <juliomaurocarvalho@hotmail.com>
2018-01-15 14:13:06 +0000

    solving filteroutthegeese.js

---
+++ filteroutthegeese.js
@@ -0,0 +1,33 @@
+
+// Write a function, gooseFilter/goose_filter/GooseFilter, that takes an array of strings as an argument and returns a filtered array containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+// geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+// For example, if this array were passed as an argument:
+
+// ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+// Your function would return the following array:
+
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.function gooseFilter (birds) {
+ 
+function gooseFilter(birds) {
+	var geese = [
+		"African",
+		"Roman Tufted",
+		"Toulouse",
+		"Pilgrim",
+		"Steinbacher"
+	];
+
+	var output = [];
+
+	for (i = 0; i < birds.length; i++) {
+		if (!geese.includes(birds[i])) {
+			output.push(birds[i]);
+		}
+	}
+
+	return output;
+}
